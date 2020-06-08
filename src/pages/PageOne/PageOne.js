81a460ad15b2f455915a1607740208c94fbd00b4
CitizenFiftyTwo <darkdroguza@hotmail.fr>
2020-06-08 14:29:07 +0200

    Remove unused import

--- src/pages/PageOne/PageOne.js
+++ src/pages/PageOne/PageOne.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "../../assets/logo.svg";
 import "./PageOne.css";
 
 class PageOne extends React.Component {
--- src/pages/PageTwo/PageTwo.js
+++ src/pages/PageTwo/PageTwo.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "../../assets/logo.svg";
 import "./PageTwo.css";
 
 class PageTwo extends React.Component {
