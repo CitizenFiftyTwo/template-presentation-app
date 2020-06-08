a08b65714560996a5451d82b0b27647ca5a3a5a1
CitizenFiftyTwo <darkdroguza@hotmail.fr>
2020-06-08 13:46:22 +0200

    Delete not relevant tests on component

--- src/pages/PageOne/PageOne.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PageOne from './PageOne';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PageOne />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
